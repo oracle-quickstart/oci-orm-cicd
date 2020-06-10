bc03d920bcb8cc7851c60e1363b978314cce3d59
Yannick Ongena <yannick.ongena@gmail.com>
2020-06-10 15:49:52 +0200

    Added license header

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,3 +1,7 @@
+# Copyright (c) 2020 Oracle and/or its affiliates.
+
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 provider "oci" {
   region    = "eu-frankfurt-1"
 }
